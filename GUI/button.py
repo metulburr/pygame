39dc865d1bf92e5076173dc612759e508f44eb07
metulburr <micahpage911@yahoo.com>
2014-01-10 01:56:51 -0500

    added fonttype option in button GUI

--- GUI/button.py
+++ GUI/button.py
@@ -1,13 +1,23 @@
 
 
 import pygame as pg
+import os
+
+
+class Font:
+    '''simulating tools'''
+    path = 'resources/fonts'
+    @staticmethod
+    def load(filename, size):
+        p = os.path.join(Font.path, filename+'.ttf')
+        return pg.font.Font(os.path.abspath(p), size)
         
 class Button:
-    def __init__(self, text='', width=100, height=10, 
+    def __init__(self, text='', width=100, height=10, fontsize=15, font=None, 
         fg=(0,0,0), bg=(255,255,255), hover=(155,155,155), command=None, border=False):
-            
+        self.font = font
         self.text_color = fg
-        self.label, self.label_rect = self.font(text, 'arial', 15)
+        self.label, self.label_rect = self.render_font(text, self.font, fontsize)
         self.bg_color = bg
         self.color = self.bg_color
         self.hover_bg_color = hover
@@ -19,8 +29,11 @@ class Button:
         self.label_rect.center = self.rect.center
         self.is_hover = False
         
-    def font(self, text, filename, size):
-        f = pg.font.SysFont(filename, size)
+    def render_font(self, text, filename, size):
+        if not filename:
+            f = pg.font.SysFont('Arial', size)
+        else:
+            f = Font.load(self.font, size)
         font = f.render(text, 1, self.text_color)
         rect = font.get_rect()
         return (font, rect)
@@ -65,13 +78,16 @@ if __name__ == "__main__":
             self.Clock = pg.time.Clock()
             self.done = False
             
-            self.btn = Button(text='Button1', width=100, height=20, 
+            self.btn = Button(text='Button1', width=100, height=20, font='impact',
                 fg=(0,0,0), bg=(155,155,155), hover=(255,255,255), command=callback)
             self.btn.rect.center = (200,100)
-            self.btn2 = Button(text='Button2', width=100, height=20, 
+            self.btn2 = Button(text='Button2', width=100, height=20, font='impact',
                 fg=(0,0,0), bg=(155,155,155), hover=(255,255,255), command=callback2)
-            self.btn2.rect.center = (200,200)
-            self.buttons = [self.btn, self.btn2]
+            self.btn2.rect.center = (200,150)
+            self.quit_btn = Button(text='Quit', width=100, height=20, font='impact',
+                fg=(255,0,0), bg=(155,155,155), hover=(255,255,255), command=self.terminate)
+            self.quit_btn.rect.center = (200,200)
+            self.buttons = [self.btn, self.btn2, self.quit_btn]
             
         def event_loop(self):
             keys = pg.key.get_pressed()
@@ -90,6 +106,9 @@ if __name__ == "__main__":
                     b.render(self.screen)
                 pg.display.update()
                 self.Clock.tick(60)
+                
+        def terminate(self):
+            self.done = True
 
 
     app = Control()
